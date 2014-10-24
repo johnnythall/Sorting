2cfd70ab12c3245b22ed1217b48342af9eb63621
Jonathan Thall <juanthall@gmail.com>
2014-10-24 15:17:42 -0700

    added quicksort

---
+++ Quicksort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int partition(int A[], int p, int r)
+{
+	int i, j;
+	i = p - 1;
+	int x = A[r];
+	
+	for(j = p; j < r; j++)
+	{
+		if(A[j] <= x)
+		{
+			i++;
+			int swap = A[i];
+			A[i] = A[j];
+			A[j] = swap;
+		}
+	}
+
+	A[r] = A[i + 1];
+	A[i + 1] = x;
+	
+	return i + 1;
+}
+
+int quicksort(int A[], int p, int r)
+{
+	if(p < r)
+	{
+		int q = partition(A,p,r);
+		quicksort(A,p, q - 1);
+		quicksort(A,q + 1,r);
+	}
+	return A;
+}
+
+int main()
+{
+	int i=0;
+	int length;
+	int A[999999];
+	int num;
+	FILE *f;
+	f=fopen("input.txt","r");
+	
+	while(fscanf(f,"%d",&num)!=EOF)
+	{
+	   A[i]=num;
+	   i++;
+
+	}
+	length = i;
+	quicksort(A, 0, length-1);
+	int x;
+	for(x=0; x<length; x++) 
+	{
+	    printf("%d\n", A[x]);
+	}
+}
